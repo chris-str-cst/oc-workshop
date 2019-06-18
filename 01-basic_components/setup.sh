c6874f9f5fe17a4e928fc7c58ed5d1a8236cc189
Christian Strobel <christian.strobel.cst@gmail.com>
2019-06-18 13:41:40 +0200

    Added loggedIn check

--- 01-basic_components/setup.sh
+++ 01-basic_components/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if you're logged in first
+if [[ "$(oc whoami 2> /dev/null)" == "" ]]; then
+  echo "You're not logged in :("
+  exit 1
+fi
+
 whaoami=$(whoami)
 first_char="$(printf '%s' "$whaoami" | cut -c1)"
 last_name=$(echo $whaoami | cut -d '.' -f2)
